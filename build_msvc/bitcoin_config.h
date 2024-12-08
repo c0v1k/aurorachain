e6e57c295fdfa59306b4025c024567179a8b6525
c0v1k <dalmat@gmail.com>
2024-12-08 14:21:38 +0100

    Update bitcoin_config.h

--- build_msvc/bitcoin_config.h
+++ build_msvc/bitcoin_config.h
@@ -15,13 +15,13 @@
 #define CLIENT_VERSION_IS_RELEASE true
 
 /* Major version */
-#define CLIENT_VERSION_MAJOR 0
+#define CLIENT_VERSION_MAJOR 1
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 21
+#define CLIENT_VERSION_MINOR 0
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 3
+#define CLIENT_VERSION_REVISION 0
 
 /* Copyright holder(s) before %s replacement */
 #define COPYRIGHT_HOLDERS "The %s developers"
