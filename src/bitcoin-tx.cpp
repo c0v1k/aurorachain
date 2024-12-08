592ed96eda155b364018252f943fa843a5cca515
c0v1k <dalmat@gmail.com>
2024-12-08 13:53:26 +0100

    Update bitcoin-tx.cpp

--- src/bitcoin-tx.cpp
+++ src/bitcoin-tx.cpp
@@ -97,9 +97,9 @@ static int AppInitRawTx(int argc, char* argv[])
 
     if (argc < 2 || HelpRequested(gArgs)) {
         // First part of help message is specific to this utility
-        std::string strUsage = PACKAGE_NAME " litecoin-tx utility version " + FormatFullVersion() + "\n\n" +
-            "Usage:  litecoin-tx [options] <hex-tx> [commands]  Update hex-encoded litecoin transaction\n" +
-            "or:     litecoin-tx [options] -create [commands]   Create hex-encoded litecoin transaction\n" +
+        std::string strUsage = PACKAGE_NAME " aurora-tx utility version " + FormatFullVersion() + "\n\n" +
+            "Usage:  aurora-tx [options] <hex-tx> [commands]  Update hex-encoded aurora transaction\n" +
+            "or:     aurora-tx [options] -create [commands]   Create hex-encoded aurora transaction\n" +
             "\n";
         strUsage += gArgs.GetHelpMessage();
 
